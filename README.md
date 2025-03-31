5143d4e4107fb40145761e8203a4e8f066457130
Damillora <developer@damillora.com>
2025-03-31 20:44:21 +0700

    fix: incomplete maildir iteration

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ overly simple mail archiving solution
 
 ## Configuration
 
-centaureissi is configured using environment variables:
+centaureissi is configured using the file `centaureissi.toml`:
 
-* `DATA_DIR`: Data directory to store images
-* `AUTH_SECRET`: Secret used to sign JWTs
-* `BASE_URL`: Accesible URL of the instance
-* `DISABLE_REGISTRATION`: Optional, disable registration on the instance
+* `data_dir`: Data directory to store images
+* `auth_secret`: Secret used to sign JWTs
+* `base_url`: Accesible URL of the instance
+* `disable_registration`: Optional, disable registration on the instance
 
 ## Contributing
 centaureissi is still incomplete, and not yet open to contributions.
--- crates/centaureissi_importer/src/maildir.rs
+++ crates/centaureissi_importer/src/maildir.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use maildirs::Maildirs;
+use maildirs::{MaildirEntry, Maildirs, MaildirsEntry};
 use tokio::fs;
 
 use crate::{
@@ -16,46 +16,48 @@ pub async fn import_maildir(
 ) -> Result<(), ImporterError> {
     println!("Importing maildir {}", maildir);
     let dir = Maildirs::new(Path::new(&maildir));
-    for mdir in dir.iter() {
+
+    let mut counter_size = 0;
+    let mut counter = 0;
+    let mut messages = Vec::<MailEntry>::new();
+    let mut paths = Vec::<String>::new();
+
+    let msgs = dir.iter().map(|s| s.maildir).flat_map(|dir| {
+        let mdir = dir.read().unwrap();
+
+        mdir.collect::<Vec<MaildirEntry>>()
+    });
+
+    let mut msgs_iter = msgs.peekable();
+
+    while let Some(msg) = msgs_iter.next() {
+        let contents = msg.read()?;
+        let file_name = msg.file_name()?;
+        let path = msg.path().to_owned().clone();
+        counter_size += contents.len();
+        counter += 1;
+        messages.push(MailEntry {
+            file_name: String::from(file_name),
+            contents: contents,
+        });
+        paths.push(String::from(path.to_str().unwrap()));
         if verbose {
-            println!("Checking folder {}", mdir.name);
+            println!("Uploading {}", file_name);
         }
-        let mut counter_size = 0;
-        let mut counter = 0;
-        let mut messages = Vec::<MailEntry>::new();
-        let mut paths = Vec::<String>::new();
+        if counter_size >= 10_000_000 || counter >= 100 || msgs_iter.peek().is_none() {
+            println!("Uploading {} emails", counter);
+            http_client.upload_message_batch(messages).await?;
+            counter_size = 0;
+            counter = 0;
+            messages = Vec::<MailEntry>::new();
 
-        if let Ok(msgs) = mdir.maildir.read() {
-            for msg in msgs {
-                let contents = msg.read()?;
-                let file_name = msg.file_name()?;
-                let path = msg.path().to_owned().clone();
-                counter_size += contents.len();
-                counter += 1;
-                messages.push(MailEntry {
-                    file_name: String::from(file_name),
-                    contents: contents,
-                });
-                paths.push(String::from(path.to_str().unwrap()));
-                if verbose {
-                    println!("Uploading {}", file_name);
-                }
-                if counter_size >= 10_000_000 || counter >= 100 {
-                    println!("Uploading {} emails", counter);
-                    http_client.upload_message_batch(messages).await?;
-                    counter_size = 0;
-                    counter = 0;
-                    messages = Vec::<MailEntry>::new();
-
-                    if delete_source {
-                        for path in paths {
-                            fs::remove_file(path).await?;
-                        }
-                    }
-
-                    paths = Vec::<String>::new();
+            if delete_source {
+                for path in paths {
+                    fs::remove_file(path).await?;
                 }
             }
+
+            paths = Vec::<String>::new();
         }
     }
 
